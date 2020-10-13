623c7aca93d9095ee7fdbdcd369677f11972f476
Lyaaaaaaaaaaaaaaa <Lya>
2020-10-13 09:50:39 +0200

    Implemented the main.py file

--- src/main.py
+++ src/main.py
@@ -12,6 +12,17 @@
 #-- Changelog:
 #--   17/09/2020 Lyaaaaa
 #--     - Cleaned the main.py (from auto generated code)
+#--
+#--   13/10/2020 Lyaaaaa
+#--     - Implemented the main.
 #---------------------------------------------------------------------------
 
+from interface import Interface
+
+
+Interface = Interface()
+
+Interface.Connect_Interface()
+Interface.Connect_Signals()
+Interface.Start_Application()
 
