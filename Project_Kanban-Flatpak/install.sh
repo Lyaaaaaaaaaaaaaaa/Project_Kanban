fe63f40fcf85ba9210012b4694be08ccddd08a50
Lyaaaaaaaaaaaaaaa <Lyaaaaaaaaaaaaaaa@users.noreply.github.com>
2021-01-15 15:42:58 +0100

    Deleted useless repo in repo

--- Project_Kanban-Flatpak/install.sh
+++
@@ -1,6 +0,0 @@
-{
-  pip install -r requirements.txt
-} || {
-  echo "If your system doesn't know the pip command you need to install python-pip or python3-pip"
-}
-
--- Project_Kanban-Flatpak/start.sh
+++
@@ -1,7 +0,0 @@
-{
-  cd src/
-  python3 main.py
-} || {
-  cd src/
-  python main.py
-}
