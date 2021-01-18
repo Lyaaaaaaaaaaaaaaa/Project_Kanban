314accc29881a809508bfc0e6079f602965012ed
Lyaaaaaaaaaaaaaaa <Lyaaaaaaaaaaaaaaa@users.noreply.github.com>
2021-01-18 15:40:34 +0100

    Updated build_application.sh
    - It now deletes the .repo and .build folders after completion.

--- build_application.sh
+++ build_application.sh
@@ -1,3 +1,4 @@
 flatpak-builder --force-clean --repo=./.repo .build io.github.lyaaaaaaaaaaaaaaa.Project_Kanban.yml
 mkdir .bundle
 flatpak build-bundle .repo .bundle/io.github.lyaaaaaaaaaaaaaaa.Project_Kanban.flatpak io.github.lyaaaaaaaaaaaaaaa.Project_Kanban master
+rm -R -f .build .repo
